27251eee8200e16b2d1f8978f1689a0c3749ff6b
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-03-10 03:29:52 -0500

    updated index and action php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php 
 
+// Document root
+// Path: public/index.php
 
 
 echo "<h1> Hello from the web root! </h1>";
 
-// Path: public/index.php
+
+
+// start the sssion
+session_start();
+
+// server agent
+echo $_SERVER['HTTP_USER_AGENT'];
+
+
+// var_dump($_SESSION);
 
