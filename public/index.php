3b3d1de7070135e424ad92d5777588e7639dfaa7
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-03-10 03:52:38 -0500

    Add token expiration logic to index.php

--- public/index.php
+++ public/index.php
@@ -17,3 +17,22 @@ echo $_SERVER['HTTP_USER_AGENT'];
 
 // var_dump($_SESSION);
 
+
+// Create a unique token 
+// Securing a session by converting 16 to 32 byes 
+
+// $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
+
+// generate a fresh token after 15 minutes 
+
+
+// $_SESSION['token_expires'] = time() + 900;
+
+// time() returns the number of seconds since January 1st, 1970
+
+
+// check if token is expired
+if (time() > $_SESSION['token_expires']) {
+    $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
+    $_SESSION['token_expires'] = time() + 900;
+}
