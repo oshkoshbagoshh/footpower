e56a4764e02d9e53353b569d856880c03ee57e60
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-03-12 16:53:09 -0500

    added dataform, database csv

---
+++ public/common/data_form.php
@@ -0,0 +1,16 @@
+<form id="data-form">
+    <div class="form-wrapper">
+        <div class="form-row">
+            <div class="control-wrapper">
+                <label for="activity-type"> Activity type </label>
+                <select id="activity-type" name="activity-type" size="=1" aria-label="Select the type of activity"></select>
+                    <option value="Walk">Walk </option>
+                    <option value="Run">Run </option>
+                    <option value="Cycle"> Cycle</option>
+                </select>
+                <!-- BOOKMARK -->
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
