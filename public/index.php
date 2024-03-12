0de22b0ae435807aca8ac9bcc3c92dd29b53b4bb
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-03-12 16:55:40 -0500

    Add app home page and main content

--- public/index.php
+++ public/index.php
@@ -36,3 +36,24 @@ if (time() > $_SESSION['token_expires']) {
     $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
     $_SESSION['token_expires'] = time() + 900;
 }
+
+<!-- APP HOME PAGE  -->
+
+<!--  -->
+<!-- TOP  -->
+<?php 
+    include_once '../private/common/initialization.php';
+    $page_title = 'Home';
+    include_once 'common/top.php';
+?>
+<!-- END TOP  -->
+<!-- MAIN  -->
+Main app content goes here
+<!-- END MAIN  -->
+<!-- BOTTOM -->
+<?php 
+    include_once 'common/sidebar.php';
+    include_once 'common/bottom.php';
+?>
+
+<!-- END BOTTOM -->
