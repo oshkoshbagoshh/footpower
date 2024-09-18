555e96a21daa5ebceaa28b547a2b0c792fb3c811
AJ Javadi <amirjavadi25@gmail.com>
2024-09-18 15:45:56 -0500

    utility

---
+++ utility/file_setup.php
@@ -0,0 +1,14 @@
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-09-18 15:25:20 
+ * @Last Modified by: AJ Javadi
+ * @Last Modified time: 2024-09-18 15:26:38
+ * @Description: file:///Applications/XAMPP/xamppfiles/htdocs/footpower/utility/file_setup.php
+ * File Setup script to create public and private directories with files and folders in them using PHP
+ */
+
+ <?php 
+
+ //TODO: set up private, public directories with subdirectories in them.
+ 
