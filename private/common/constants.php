2552977fdc065bb11e660de62a8e6f7809af4933
AJ Javadi <amirjavadi25@gmail.com>
2024-09-18 17:17:52 -0500

    Add constants.php file for storing private constants

---
+++ private/common/constants.php
@@ -0,0 +1,27 @@
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-09-18 16:11:52 
+ * @Last Modified by: AJ Javadi
+ * @Last Modified time: 2024-09-18 16:19:26
+ * @Description: file:///Applications/XAMPP/xamppfiles/htdocs/footpower/private/common/constants.php
+ * used to store our private constants
+ */
+
+
+<?php 
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD','');
+define('DATABASE', 'footpower');
+
+
+// DATABASE CONFIGURATION
+echo "-- DATABASE CONFIGURATION --\n";
+echo "Host is " . constant('HOST') . "\n";
+echo "User is " . constant('USER') ."\n";
+echo "Password is " . constant('PASSWORD') ."\n";
+echo "Database is " . constant('DATABASE') ."\n";
+echo "----------\n";
+
