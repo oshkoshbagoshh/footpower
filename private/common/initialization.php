83c3211e8aef33274e4e79cb4dcbd264cf9a08f4
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-09-18 17:45:41 -0500

    Add initialization.php file for web app initialization

---
+++ private/common/initialization.php
@@ -0,0 +1,19 @@
+/*
+ * @Author: AJ Javadi 
+ * @Email: amirjavadi25@gmail.com
+ * @Date: 2024-09-18 17:35:45 
+ * @Last Modified by: AJ Javadi
+ * @Last Modified time: 2024-09-18 17:36:56
+ * @Description: file:///Applications/XAMPP/xamppfiles/htdocs/footpower/private/common/initialization.php
+ * Back-end chores for web app initialization
+ */
+
+ <?php
+
+    //  make sure we see all the errors and warnings
+    error_reporting(E_ALL | E_STRICT);
+
+    // start a session
+    session_start();
+
+    
