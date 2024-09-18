aefb4b8c0337ce12482d98f333560202c095373a
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-09-18 18:50:55 -0500

    Refactor index.php for web app structure and include common files

---
+++ public/index.php
@@ -0,0 +1,38 @@
+/*
+* @Author: AJ Javadi
+* @Email: amirjavadi25@gmail.com
+* @Date: 2024-09-18 18:46:51
+* @Last Modified by: AJ Javadi
+* @Last Modified time: 2024-09-18 18:49:52
+* @Description: file:///Applications/XAMPP/xamppfiles/htdocs/footpower/public/index.php
+* Web Root Directory
+*/
+
+<!-- TOP -->
+<?php
+
+include_once('../private/common/initialization.php');
+$page_title = 'Home';
+include_once('./common/top.php');
+?>
+<!-- TOP ENDS -->
+
+<!-- MAIN CONTENT -->
+<p> Main app content goes here</p>
+
+<!-- END MAIN -->
+
+
+<!-- SIDE BAR  -->
+<?php
+
+include_once('./common/sidebar.php');
+?>
+<!-- END SIDEBAR -->
+
+<!-- FOOTER / BOTTOM -->
+<?php
+
+include_once('./common/bottom.php');
+?>
+<!-- END FOOTER / BOTTOM -->
