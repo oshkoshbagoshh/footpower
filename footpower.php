73cab623c35f58c5803e36bd35a5af1ab4f3243d
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-09-18 21:54:35 -0500

    Refactor footpower.sql to update the data type of the 'distance' column

---
+++ footpower.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `footpower`
+ */
+
+/* `footpower`.`activities` */
+$activities = array(
+  array('activity_id' => '1','log_id' => '2','type' => 'running','date' => '2024-09-18','distance' => '2','duration' => '00:00:15')
+);
+
+/* `footpower`.`logs` */
+$logs = array(
+  array('log_id' => '2','user_id' => '1','creation_date' => '2024-09-18 17:13:12')
+);
+
+/* `footpower`.`users` */
+$users = array(
+  array('user_id' => '1','username' => 'zztest1','password' => 'zztest1','distance_unit' => 'miles','verification_code' => '1234','verified' => '1','creation_date' => '2024-09-18 17:12:53')
+);
--- footpower.sql
+++ footpower.sql
@@ -32,7 +32,7 @@ CREATE TABLE `activities` (
   `log_id` int(11) NOT NULL,
   `type` varchar(25) NOT NULL,
   `date` date NOT NULL,
-  `distance` decimal(10,0) NOT NULL,
+  `distance` decimal(10,6) NOT NULL,
   `duration` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='used to log records to the user''s foot-propelled activities';
 
@@ -151,3 +151,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
