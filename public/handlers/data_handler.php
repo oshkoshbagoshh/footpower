7840d3dc22d9550ea8c2261f766f65923da9440f
AJ Javadi <amirjavadi25@gmail.com>
2024-10-16 22:37:41 -0500

    data handlers and classes

--- public/handlers/data_handler.php
+++ public/handlers/data_handler.php
@@ -25,14 +25,65 @@
     }
 
     // Make sure a token value was passed
+    elseif (!isset($_POST['token'])) {
+        $server_results['status'] = 'error';
+        $server_results['message'] = 'Error: Invalid action!';
+    }
 
 
-    // Make sure the token is legit
+    // Make sure the token is legit (session token needs to match POST token
+    elseif ($_SESSION['token'] !== $_POST['token']) {
+        $server_results['status'] = 'error';
+        $server_results['message'] = 'Timeout Error! <p>Please refresh the page and try again.</p>';
+    }
 
 
     // If we get this far, all is well, so go for it
+    else {
+
+        // Create a new Data object
+        $data = new Data($mysqli);
+
+        // Pass the data verb to the appropriate method
+        switch ($_POST['data-verb']) {
+
+            // create a new data item
+            case 'create':
+                $server_results = json_decode($data->createData());
+                break;
+
+            // read all data items
+            case 'read-all-data':
+                $server_results = json_decode($data->readAllData());
+                break;
+
+            // read one data item
+            case 'read-data-item':
+                $server_results = json_decode($data->readDataItem());
+                break;
+
+            // update a data item
+            case 'update':
+                $server_results = json_decode($data->updateData());
+                break;
+
+            // delete a data item
+            case 'delete':
+                $server_results = json_decode($data->deleteData());
+                break;
+
+            default:
+                $server_results['status'] = 'error';
+                $server_results['message'] = 'Error: Unknown data verb!';
 
 
+
+        }
+    }
+
+    // create and then output the JSON data
+    $JSON_data = json_encode($server_results, JSON_HEX_APOS | JSON_HEX_QUOT);
+    echo $JSON_data;
 
 
 
